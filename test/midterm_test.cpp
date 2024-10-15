1197d7cc22dde9c075f8c8676f35f2a3637ec751
Michael Reid <cybercritter@comcast.net>
2024-10-15 14:39:19 -0600

    workflow final

--- test/midterm_test.cpp
+++ test/midterm_test.cpp
@@ -1,4 +1,4 @@
 #include <gtest/gtest.h>
 
 TEST (midterm_test, test_true) { ASSERT_TRUE (true); }
-TEST (midterm_test, test_faile) { ASSERT_TRUE (false); }
+TEST (midterm_test, test_faile) { ASSERT_FALSE (false); }
